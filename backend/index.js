39685328d83469792b8e0b94ba4436c083a4ef6b
nikhilknvr-12 <nikhilkn.vr@gmail.com>
2025-10-29 23:18:16 +0530

    Middleware to handle CORS

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+// Middleware to handle cors
+app.use(cors(
+  {
+    origin: process.env.FRONT_END_URL || 'http://localhost:5173', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }
+));
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
