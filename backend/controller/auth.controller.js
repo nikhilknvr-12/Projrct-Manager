927d64d4185411f6f9f4f971ff298bfbe998dc02
nikhilknvr-12 <nikhilkn.vr@gmail.com>
2025-10-30 14:36:32 +0530

    Signup user api route

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,49 @@
+import User from "../models/user.model.js"
+import bcryptjs from "bcryptjs"
+
+export const signup = async (req, res, next) => {
+  const { name, email, password, profileImageUrl, adminJoinCode } = req.body
+
+  if (
+    !name ||
+    !email ||
+    !password ||
+    name === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return next(errorHandler(400, "All fields are required"))
+  }
+
+  //   Check if user already exists
+  const isAlreadyExist = await User.findOne({ email })
+
+  if (isAlreadyExist) {
+    return next(errorHandler(400, "User already exists"))
+  }
+
+  //   check user role
+  let role = "user"
+
+  if (adminJoinCode && adminJoinCode === process.env.ADMIN_JOIN_CODE) {
+    role = "admin"
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10)
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+    profileImageUrl,
+    role,
+  })
+
+  try {
+    await newUser.save()
+
+    res.json("Signup successful")
+  } catch (error) {
+    next(error.message)
+  }
+}
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,12 +1,33 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  profileImageUrl: { type: String, default: 'https://img.freepik.com/premium-vector/user-icon-icon_1076610-59410.jpg' },
-  roles: { type: String, enum:['admin','user'], default: ['user'] },
-}, { timestamps: true });
+  name: {
+    type: String,
+    required: true,
+  },
+    email: {    
+    type: String,
+    required: true,
+    unique: true,
+    },
+  password: {
+    type: String,
+    required: true,
+    },
+    profileImageUrl: {
+    type: String,
+    default: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    },
+    role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+    },
+},
+{
+  timestamps: true,
+}
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
