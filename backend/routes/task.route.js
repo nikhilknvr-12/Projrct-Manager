cf10821329dcc817d1f8044ba470ea6a4374436f
nikhilknvr-12 <nikhilkn.vr@gmail.com>
2025-11-01 14:41:26 +0530

    get dashboard data

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,17 +1,18 @@
 import express from 'express'
 import { adminOnly, verifyToken } from '../utils/verifyUser.js'
-import { createTask, deleteTask, getTaskById, getTasks, updateTask, updateTaskChecklist, updateTaskStatus } from '../controller/task.controller.js'  
+import { createTask, deleteTask, getDashboardData, getTaskById, getTasks, updateTask, updateTaskChecklist, updateTaskStatus } from '../controller/task.controller.js'  
 
 const router= express.Router()
 
 router.post("/create", verifyToken, adminOnly, createTask )
 router.get("/", verifyToken, getTasks)
-
+router.get("/dashboard-data", verifyToken,adminOnly, getDashboardData)
 router.get("/:id",verifyToken, getTaskById)
 router.put("/:id", verifyToken, updateTask)
 router.delete("/:id", verifyToken, adminOnly, deleteTask )
 
 router.put("/:id/status", verifyToken, updateTaskStatus)
 router.put("/:id/todo", verifyToken, updateTaskChecklist)
+
 
 export default router
